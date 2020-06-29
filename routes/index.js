d19e118a3e36eb2809c3edad54781dfc00f3fa61
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-28 22:02:07 -0700

    notes added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const { Router } = require("express");
+//bring in router
 const router = require('express')
 
+//define routes in folder
 router.use('/api', require('./bookRoutes.js'))
 router.use('/api', require('./googleRoutes.js'))
 
-
+//export to server
 module.exports = router
