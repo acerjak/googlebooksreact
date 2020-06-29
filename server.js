278d1ef33ba386e2eac0256b6d739af791787206
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-28 21:50:03 -0700

    server notes added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+//bring in dotenv
 require('dotenv').config()
+//bring in express
 const express = require('express')
+//bring in join method from path
 const { join } = require('path')
 
+//create express app
 const app = express()
 
+//middleware set-up, full directory name, client, and build
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -15,9 +20,13 @@ app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
 })
 
+//set up mongoose connection for deployment
+//use mongo or local
 require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+  //start app listen for server on PORT or 3001
   .then(() => app.listen(process.env.PORT || 3001))
+  //catch any errors
   .catch(err => console.error(err))
