a67d9a193135854b5c9bb7624b417d5422139815
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-28 23:07:17 -0700

    react router started

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+
+          </Route>
+          <Route path='/saved'>
+            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
